1e0ceb63adc3c5d01b8ea5f0fd4f1a414e9fc51f
Giavinh <glam01@uoguelph.ca>
2017-09-17 05:36:28 -0400

    -a

--- LinkedListAPI.c
+++ LinkedListAPI.c
@@ -49,11 +49,14 @@ Node * initializeNode(void * data)
 
 void insertFront(List* list, void* toBeAdded)
 {
-    Node * temp;
-    temp = (Node *)toBeAdded;
-    list->head->previous = temp;
-    temp->next = list->head;
-    temp->previous = NULL;
+    //Node * temp = list;
+    Node * newNode = initializeNode(toBeAdded);
+    list->head->previous = newNode;
+    newNode->next = list->head;
+    newNode->previous = NULL;
+    list->head = newNode;
+    //list->head
+    //newNode->previous = NULL;
     //(List)toBeAdded->next = list;
     //(List)toBeAdded->previous = NULL;
     return;
@@ -63,12 +66,12 @@ void insertFront(List* list, void* toBeAdded)
 
 void insertBack(List* list, void* toBeAdded)
 {
-    Node * temp;
-    temp = (Node *)toBeAdded;
-    list->tail->next = temp;
-    temp->previous = list->tail;
-    temp->next = NULL;
-    list->tail = temp;
+    //Node * temp = list;
+    Node * newNode = initializeNode(toBeAdded);
+    list->tail->next = newNode;
+    newNode->previous = list->tail;
+    newNode->next = NULL;
+    list->tail = newNode;
     //temp->next = NULL;
     //temp->previous = list;
     return;
@@ -80,7 +83,7 @@ void clearList(List* list)
     Node * temp = list->head;
     while (temp->next != NULL)
     {
-        free(list->head->data);
+        free(temp->data);
         temp = temp->next;
     }
     free(list->tail);
@@ -91,9 +94,13 @@ void clearList(List* list)
 void insertSorted(List* list, void* toBeAdded)
 {
     Node * temp = list->head;
-    if (list->compare(list->head->data, toBeAdded) >= 0)
+    Node * newNode = initializeNode(toBeAdded);
+    while (temp->next != NULL)
     {
-
+        if (list->compare(list->head->data, toBeAdded) <= 0)
+        {
+            //
+        }
     }
     return;
 }
@@ -116,3 +123,23 @@ void* getFromBack(List list)
  }
 */
 
+/*
+char* toString(List list)
+{
+
+}
+*/
+
+/*
+ListIterator createIterator(List list)
+{
+
+}
+*/
+
+/*
+void * nextElement(ListIterator * iter)
+{
+
+}
+*/
